1feda3cc24972e196a2f50df34ae80e0048df970
SirMphuthi <kgotsomphuthi@gmail.com>
2025-06-10 08:10:16 +0000

    Update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,16 +4,16 @@
  * the user's name, and a closing message.
  */
 
-process.stdout.write('Welcome to ALX, what is your name?\n');
+console.log('Welcome to ALX, what is your name?'); // Changed to console.log()
 
 process.stdin.setEncoding('utf8'); // Set encoding for stdin
 
 process.stdin.on('data', (data) => {
   // Trim whitespace (like newline characters) from the input
   const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+  console.log(`Your name is: ${name}`); // Changed to console.log()
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  console.log('This important software is now closing'); // Changed to console.log()
 });
