302cbbd45fbf931d9ec1f42989e01463e127f8a3
SirMphuthi <kgotsomphuthi@gmail.com>
2025-06-10 19:17:57 +0000

    Task 6

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,21 +1,11 @@
-#!/usr/bin/env node
-const express = require('express'); // Import the Express.js framework
+const express = require('express');
 
-// Create an Express application instance
 const app = express();
 
-// Define a route for the root endpoint '/'
 app.get('/', (req, res) => {
-  // Send "Hello ALX!" as the response for GET requests to the root path
-  res.send('Hello ALX!');
+  res.status(200).send('Hello Holberton School!');
 });
 
-// The server should listen on port 1245
-const PORT = 1245;
-app.listen(PORT, () => {
-  // Optional: Log message to console when server starts for local testing
-  // console.log(`Express server listening on port ${PORT}`);
-});
+app.listen(1245);
 
-// Export the Express app instance for testing purposes
 module.exports = app;
