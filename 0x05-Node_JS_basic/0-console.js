bdb46bb12dce82b918486b28ba0ab12554da6461
SirMphuthi <kgotsomphuthi@gmail.com>
2025-06-10 07:48:14 +0000

    Update

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,9 +1,10 @@
+#!/usr/bin/env node
 /**
  * Displays a message to the STDOUT.
  * @param {string} message The message to display.
  */
 function displayMessage(message) {
-  process.stdout.write(message + '\n');
+  process.stdout.write(message + '\n'); // Added a semicolon
 }
 
-module.exports = displayMessage;
+module.exports = displayMessage; // Added a semicolon
